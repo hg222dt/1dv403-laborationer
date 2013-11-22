0be4b6cdce4fddae16209578394698f48d9fcd3b
Henrik Gudmundsson <hg222dt@student.lnu.se>
2013-11-22 06:09:52 -0500

    Har lagt itll kommentarer

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -54,16 +54,21 @@ var makePerson = function(persArr) {
 	var nameArray = new Array();
 	var i = 0;
 	
+	//Ittererar genom vårt array och skriver alla åldrar till ett utomstående array, samt alla namn till ett annat utomstående array.
 	while(i < persArr.length){
 	    ageArray[i] = persArr[i].age;
 	    nameArray[i] = persArr[i].name;
 	    i++;
 	}
 	
+	//Initierar och sätter egenskaper i persons för minsta och största ålder från vårt array med åldrar.
     persons.minAge = Math.min.apply(null, ageArray);
 	persons.maxAge = Math.max.apply(null, ageArray);
+	
+	//Initierar egenskap i persons för medelålder.
 	persons.averageAge = 0;
 	
+	//Räknar under ut vår medelålder
 	i = 0;
 	var totalAge = 0;
 	while(i < ageArray.length){
@@ -80,8 +85,6 @@ var makePerson = function(persArr) {
 	persons.names = nameArray.join(", ");
 
     return persons;
-    
-	
 };
 
 var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
