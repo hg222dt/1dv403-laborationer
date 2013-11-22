1cfafed8aafb18429ddc4ce4a41ad225861acb50
Henrik Gudmundsson <hg222dt@student.lnu.se>
2013-11-22 06:06:55 -0500

    Har lagt till en array bestående av objekt med namn och åldrar, samt anrop av funktionen vi skapat

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -83,3 +83,9 @@ var makePerson = function(persArr) {
     
 	
 };
+
+var data = [{name: "John Häggerud", age: 37}, {name: "Johan Leitet", age: 36}, {name: "Mats Loock", age: 46}];
+
+var result = makePerson(data);
+
+console.log(result);
