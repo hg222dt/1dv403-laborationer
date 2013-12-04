7c8d187564ebe3c4a76526e1a2d3be874033641c
Henrik Gudmundsson <hg222dt@student.lnu.se>
2013-12-04 08:53:32 -0500

    Programmet skriver nu ut meddleanden ovanför textinmatningen.

--- 2-labbymezzage/script/MessageBoard.js
+++ 2-labbymezzage/script/MessageBoard.js
@@ -8,13 +8,23 @@ var MessageBoard = {
             var textInput = document.getElementById("textInputArea").value;
             var mess = new Message(textInput, new Date());
             MessageBoard.messages.push(mess);
-            alert(MessageBoard.messages[0]);
+            //alert(MessageBoard.messages[0]);
             MessageBoard.renderMessages();
-        };
             
+            document.getElementById("textInputArea").value="";
+            
+            event.preventDefault();
+        };
+        
         var button = document.getElementById("skickaknapp");
         
         button.addEventListener("click", createMessage, false);
+        
+        
+        var eraseButton = document.getElementsByClass("TaBortKnapp");
+        
+        eraseButton.addEventListener("click", MessageBoard.removeMessage(this), false);
+        
     },  
     
     renderMessages: function () {
@@ -22,17 +32,33 @@ var MessageBoard = {
         document.getElementById("messageBoard").innerHTML = "";
         
         //Render all messages
-        for(var i=0; i<MessageBoard.message.length; i++){
+        for(var i=0; i<MessageBoard.messages.length; i++){
             MessageBoard.renderMessage(i);
         }
-        
     },
     
     renderMessage: function (messageID) {
         //document.getElementById("messageBoard").innerHTML = MessageBoard.messages[messageID].getText();
         
+        
         var messageBoard = document.querySelector("#messageBoard");
+        var atag1 = document.createElement("a");
+        var atag2 = document.createElement("a");
+        
+        atag1.setAttribute("href", "#");
+        atag2.setAttribute("href", "#");
+        
         var p = document.createElement("p");
+        
+        
+        var imgClose = document.createElement("img");
+        imgClose.setAttribute("src", "cross.png");
+        imgClose.setAttribute("class", "TaBortKnapp")
+        
+        
+        var imgTime = document.createElement("img");
+        imgTime.setAttribute("src", "clock.png");
+        
         
         p.innerHTML = MessageBoard.messages[messageID].getHTMLText();
         
@@ -40,8 +66,18 @@ var MessageBoard = {
         //p.appendChild(text);
         
         messageBoard.appendChild(p);
-
+        
+        messageBoard.appendChild(atag1);
+        atag1.appendChild(imgClose);
+        messageBoard.appendChild(atag2);
+        atag2.appendChild(imgTime);
+        
+    },
+    
+    removeMessage: function () {
+        
     }
+    
 }
 
 window.onload = MessageBoard.init;
