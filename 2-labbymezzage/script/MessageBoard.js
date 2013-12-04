9223f00325fa4f17411bd195e644cb0c5f3d3456
Henrik Gudmundsson <hg222dt@student.lnu.se>
2013-12-04 06:03:19 -0500

    Har försökt lägga till en funkationalitet att meddelanden ska visas i html-dokumentet

--- 2-labbymezzage/script/MessageBoard.js
+++ 2-labbymezzage/script/MessageBoard.js
@@ -9,16 +9,39 @@ var MessageBoard = {
             var mess = new Message(textInput, new Date());
             MessageBoard.messages.push(mess);
             alert(MessageBoard.messages[0]);
+            MessageBoard.renderMessages();
         };
             
         var button = document.getElementById("skickaknapp");
         
         button.addEventListener("click", createMessage, false);
-        
-
-       
+    },  
     
-    }  
+    renderMessages: function () {
+        //remove all messages
+        document.getElementById("messageBoard").innerHTML = "";
+        
+        //Render all messages
+        for(var i=0; i<MessageBoard.message.length; i++){
+            MessageBoard.renderMessage(i);
+        }
+        
+    },
+    
+    renderMessage: function (messageID) {
+        //document.getElementById("messageBoard").innerHTML = MessageBoard.messages[messageID].getText();
+        
+        var messageBoard = document.querySelector("#messageBoard");
+        var p = document.createElement("p");
+        
+        p.innerHTML = MessageBoard.messages[messageID].getHTMLText();
+        
+        //var text = document.createTextNode(MessageBoard.messages[messageID].getText());
+        //p.appendChild(text);
+        
+        messageBoard.appendChild(p);
+
+    }
 }
 
 window.onload = MessageBoard.init;
